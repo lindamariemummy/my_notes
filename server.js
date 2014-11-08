751ebf490cdadf7d622ec6974bc1e46d51d46b27
lindamariemummy <lindamariemummy@gmail.com>
2014-11-07 18:48:04 -0800

    add mongodb

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ var app = express();
 
 app.use(bodyparser.json());
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
+
+//code from https://github.com/mongolab/mongodb-driver-examples/blob/
+// master/nodejs/mongooseSimpleExample.js
+var mongodbUri = 'mongodb://heroku_app31292297:hfvmdkr6ndssg4cbimge6oinam@ds051740.mongolab.com:51740/heroku_app31292297';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+mongoose.connect(mongooseUri, options);
+
+
+
+//mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/notes_development');
 
 require('./routes/notes_routes')(app);
 
