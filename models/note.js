81e53c2fe4a20bcc9887bbe4b5c997a55f0a0080
lindamariemummy <lindamariemummy@gmail.com>
2014-11-07 22:09:28 -0800

    refuses to write if author isn't Linda

--- models/note.js
+++ models/note.js
@@ -1,7 +1,17 @@
 var mongoose = require('mongoose');
 
 var noteSchema = mongoose.Schema({
-  noteBody: 'String'
+  noteBody: 'String',
+  noteAuthor: 'String'
 });
+
+var Notes = mongoose.model('Note', noteSchema);
+
+
+//thanks to the Mongoose validation docs:
+// http://mongoosejs.com/docs/2.7.x/docs/validation.html
+noteSchema.path('noteAuthor').validate(function (v) {
+  return v == "Linda";
+}, 'not by Linda');
 
 module.exports = mongoose.model('Note', noteSchema);
--- test/api/notes_test.js
+++ test/api/notes_test.js
@@ -9,23 +9,35 @@ var expect = chai.expect;
 
 describe('basic notes crud', function() {
   var id;
-  it('should be able to create a note', function(done) {
+  it('should be able to create a note with author of Linda', function(done) {
     chai.request('https://fathomless-refuge-1297.herokuapp.com')
     //chai.request('http://localhost:3000')
     .post('/api/notes')
-    .send({noteBody: 'hello world'})
+    .send({noteBody: 'new note', noteAuthor: 'Linda'})
     .end(function(err, res) {
       expect(err).to.eql(null);
       //console.log(res);
-      //expect(res.body.noteBody).to.eql('hello world');
-      //expect(res.body).to.have.property('_id');
+      expect(res.body.noteAuthor).to.eql('Linda');
+      expect(res.body).to.have.property('_id');
       id = res.body._id;
       done();
     });
   });
+  it('should fail to create a note if author is not Linda', function(done) {
+    chai.request('https://fathomless-refuge-1297.herokuapp.com')
+    //chai.request('http://localhost:3000')
+    .post('/api/notes')
+    .send({noteBody: 'Hello I am a new note', noteAuthor: 'Freddy'})
+    .end(function(err, res) {
+      console.log(res);
+      expect(res.statusCode).to.eql(500);
+      done();
+    });
+  });
 
-/*  it('should be able to get an index', function(done) {
-    chai.request('http://localhost:3000')
+  it('should be able to get an index', function(done) {
+    //chai.request('http://localhost:3000')
+    chai.request('https://fathomless-refuge-1297.herokuapp.com')
     .get('/api/notes')
     .end(function(err, res) {
       expect(err).to.eql(null);
@@ -35,33 +47,36 @@ describe('basic notes crud', function() {
   });
 
   it('should be able to get a single note', function(done) {
-    chai.request('http://localhost:3000')
+    //chai.request('http://localhost:3000')
+    chai.request('https://fathomless-refuge-1297.herokuapp.com')
     .get('/api/notes/' + id)
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.noteBody).to.eql('hello world');
+      expect(res.body.noteBody).to.eql('testing notes');
       done();
     });
   });
 
   it('should be able to update a note', function(done) {
-    chai.request('http://localhost:3000')
+    //chai.request('http://localhost:3000')
+    chai.request('https://fathomless-refuge-1297.herokuapp.com')
     .put('/api/notes/' + id)
-    .send({noteBody: 'new note body'})
+    .send({noteBody: 'new note', noteAuthor: 'Linda'})
     .end(function(err, res) {
       expect(err).to.eql(null);
-      expect(res.body.noteBody).to.eql('new note body');
+      expect(res.body.noteBody).to.eql('new note');
       done();
     });
   });
 
   it('should be able to destroy a note', function(done) {
-    chai.request('http://localhost:3000')
+    //chai.request('http://localhost:3000')
+    chai.request('https://fathomless-refuge-1297.herokuapp.com')
     .delete('/api/notes/' + id)
     .end(function(err, res) {
       expect(err).to.eql(null);
       expect(res.body.msg).to.eql('success!');
       done();
     });
-  });*/
+  });
 });
